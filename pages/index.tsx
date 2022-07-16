620150ad75b40661587e2c1a1a7874c202aa91ac
Junaidhassan99 <61021884+Junaidhassan99@users.noreply.github.com>
2022-07-16 13:45:17 +0500

    app bar completed

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,13 @@ import Image from "next/image";
 import { Fragment } from "react";
 import styles from "../styles/Home.module.css";
 
+const AppBarButton: React.FC<{
+  title: string;
+  className?: string;
+}> = ({ title, className = "" }) => {
+  return <button className={`mx-4 ${className}`}>{title}</button>;
+};
+
 const Home: NextPage = () => {
   return (
     <Fragment>
@@ -12,13 +19,18 @@ const Home: NextPage = () => {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       {/* Appbar */}
-      <div className="flex flex-row">
-        <div>Urbane</div>
-        <div>Home</div>
-        <div>Services</div>
-        <div>Reviews</div>
-        <div>Pricing</div>
-        <div>Download App</div>
+      <div className="horizontal-padding flex flex-row justify-between items-center py-6">
+        <div className="primary-text-color text-2xl font-semibold">Urbane</div>
+        <div className="flex flex-row">
+          <AppBarButton title="Home" />
+          <AppBarButton title="Services" />
+          <AppBarButton title="Reviews" />
+          <AppBarButton title="Pricing" />
+          <AppBarButton
+            title="Download App"
+            className="primary-background-color rounded-md px-2 py-1"
+          />
+        </div>
       </div>
     </Fragment>
   );
