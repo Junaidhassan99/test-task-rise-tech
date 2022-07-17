60dd4638fafdc37ccd120d30b014ca719dbe1b53
Junaidhassan99 <61021884+Junaidhassan99@users.noreply.github.com>
2022-07-17 11:15:54 +0500

    brand icons working

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,9 +5,11 @@ import { Fragment } from "react";
 import styles from "../styles/Home.module.css";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAppleAlt } from "@fortawesome/free-solid-svg-icons";
-
-// import { FaRocket } from '../public/apple-brands.svg ';
+import {
+  faApple,
+  faGooglePay,
+  faGooglePlay,
+} from "@fortawesome/free-brands-svg-icons";
 
 const AppBarButton: React.FC<{
   title: string;
@@ -46,6 +48,18 @@ const ButtonCard: React.FC<{
   );
 };
 
+const AppleIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 384 512"
+      className="text-red-500"
+    >
+      <path d="M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 81.2c12.8 36.7 59 126.7 107.2 125.2 25.2-.6 43-17.9 75.8-17.9 31.8 0 48.3 17.9 76.4 17.9 48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 24.8-61.9 24-72.5-24.1 1.4-52 16.4-67.9 34.9-17.5 19.8-27.8 44.3-25.6 71.9 26.1 2 49.9-11.4 69.5-34.3z" />
+    </svg>
+  );
+};
+
 const Home: NextPage = () => {
   return (
     <Fragment>
@@ -53,6 +67,7 @@ const Home: NextPage = () => {
         <title>Urbane</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
+
       {/* Appbar */}
       <section>
         <div className="horizontal-padding flex flex-row justify-between items-center py-6">
@@ -86,13 +101,17 @@ const Home: NextPage = () => {
             <div className="py-2 flex flex-row">
               <ButtonCard>
                 <div className="flex flex-row items-center">
-                  {/* <Image src="/../public/apple-brands.svg" alt={'oka'} height={30} width={30} /> */}
-                  {/* <FontAwesomeIcon icon="fa-brands fa-apple" /> */}
-                  <div>App Store</div>
+                  <FontAwesomeIcon icon={faApple} size={"lg"} />
+                  <div className="pl-2">App Store</div>
                 </div>
               </ButtonCard>
               <div className="mx-2" />
-              <ButtonCard>Google Play</ButtonCard>
+              <ButtonCard>
+                <div className="flex flex-row items-center">
+                  <FontAwesomeIcon icon={faGooglePlay} size={"lg"} />
+                  <div className="pl-2">Google Play</div>
+                </div>
+              </ButtonCard>
             </div>
           </div>
         </ComponentWithBackgroundImage>
